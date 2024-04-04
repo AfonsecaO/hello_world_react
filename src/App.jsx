e5fc5b3f928c16df54cca5c5cd692c04bd316d30
Alan Fonseca <afonsecao@outlook.com>
2024-04-03 21:41:00 -0500

    Hello world with react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola, soy Alan Fonseca</h1>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <p>Para más información <a href="https://alanfonseca.com"> visitanos</a></p>
+    </>
+  )
+}
+
+export default App
